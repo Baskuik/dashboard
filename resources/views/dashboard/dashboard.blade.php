874ac310d48f264dc11a518f38f533e377eb8a21
Baskuik <97105236@st.deltion.nl>
2026-04-13 15:42:09 +0200

    sprint 3 commitsq

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -140,7 +140,7 @@
                             <svg class="w-6 h-6 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M12 9v2m0 4v2m0 6v2M7 5h10a2 2 0 012 2v10a2 2 0 01-2 2H7a2 2 0 01-2-2V7a2 2 0 012-2z" />
-                            </svg>
+                            </svg> 
                         </div>
                     </div>
 
